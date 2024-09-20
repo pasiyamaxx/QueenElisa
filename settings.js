57f395699676cc6335ad8ed58752b411becf07d4
pasiyamaxx <130438278+pasiyamaxx@users.noreply.github.com>
2024-09-20 21:28:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: 'QUEENELISA;;;==QUD5kafhGOuhTdChGOmJWTz8EVjtGaIx2Y4UFazQ0S3xEOyRjSOpkT6ZVYjskWKp2U5I1bpwd=94712735749',
+  SESSION_ID: 'QUEENELISA;;;==QVINEMpN1SihEUjh2YuV1b6dVdhFXZr90UUxmbyUkS4Rzc5ZjY6VENZl1XjsERSZVe0JVTpwd=94712735749',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
